8a2b974927bfa4be11ea940b7b2dcae31cc9f27b
vaprvsapr <vaprvsapr@mail.ru>
2026-04-03 12:54:17 +0300

    Added EventsController.

--- EventManager/Program.cs
+++ EventManager/Program.cs
@@ -1,5 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errors = context.ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage);
+
+            var apiResult = new EventManager.Models.ApiResult
+            {
+                Success = false,
+                StatusCode = System.Net.HttpStatusCode.BadRequest,
+                Message = string.Join("; ", errors),
+                DateTime = DateTime.Now
+            };
+
+            return new Microsoft.AspNetCore.Mvc.BadRequestObjectResult(apiResult);
+        };
+    });
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -10,5 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
