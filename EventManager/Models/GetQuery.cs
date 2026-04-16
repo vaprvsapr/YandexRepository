0873c3f6d6dcd27a3f38f589af719514e5e87ebd
vaprvsapr <vaprvsapr@mail.ru>
2026-04-16 18:12:15 +0300

    Added GetQuery validation.

--- EventManager/Models/GetQuery.cs
+++ EventManager/Models/GetQuery.cs
@@ -2,7 +2,7 @@
 
 using System.ComponentModel.DataAnnotations;
 
-public class GetQuery
+public class GetQuery : IValidatableObject
 {
     public string? Title { get; init; } = null;
 
@@ -10,7 +10,18 @@ public class GetQuery
 
     public DateTime? To { get; init; } = null;
 
+    [Range(1, int.MaxValue, ErrorMessage = "Page must be greater than 0.")]
     public int Page { get; init; } = 1;
 
+    [Range(1, 100, ErrorMessage = "PageSize must be between 1 and 100.")]
     public int PageSize { get; init; } = 10;
+
+    IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+    {
+        if (From != null && To != null && From >= To)
+            yield return new ValidationResult(
+                "Время окончания события должно быть позже времени начала.",
+                [nameof(From)]
+            );
+    }
 }
