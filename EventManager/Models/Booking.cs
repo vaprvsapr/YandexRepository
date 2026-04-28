308f056af28aec955839d7fdf4dbda46daa946bb
vaprvsapr <vaprvsapr@mail.ru>
2026-04-28 16:01:47 +0300

    Добавил класс Booking.

---
+++ EventManager/Models/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace EventManager.Models;
+
+public class Booking
+{
+    public required Guid Id { get; init; }
+    public required int EventId { get; init; }
+    public required BookingStatus Status { get; set; }
+    public required DateTime CreatedAt { get; init; }
+    public DateTime? ProcessedAt { get; set; }
+}
