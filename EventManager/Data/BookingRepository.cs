089868d95dc6147e46d204d7942734a8e38ddd9b
vaprvsapr <vaprvsapr@mail.ru>
2026-04-28 17:11:43 +0300

    Добавил BookingRepository.

---
+++ EventManager/Data/BookingRepository.cs
@@ -0,0 +1,38 @@
+﻿using EventManager.Interfaces;
+using EventManager.Models;
+
+namespace EventManager.Data;
+
+public class BookingRepository : IRepository<Booking>
+{
+    private List<Booking> _bookings = [];
+    public void Add(Booking entityToAdd)
+    {
+        _bookings.Add(entityToAdd);
+    }
+
+    public void Delete(Booking entityToDelete)
+    {
+        _bookings.Remove(entityToDelete);
+    }
+
+    public IReadOnlyCollection<Booking> GetAll()
+    {
+        return _bookings.AsReadOnly();
+    }
+
+    public Booking? GetById(int id)
+    {
+        return _bookings.FirstOrDefault(b => b.Id == id);
+    }
+
+    public void Update(int id, Booking entityToUpdate)
+    {
+        var booking = GetById(id);
+        if (booking != null)
+        {
+            _bookings.Remove(booking);
+            _bookings.Add(entityToUpdate);
+        }
+    }
+}
