bfaf72151821c5e86404cf1104961e9f3af4c8ad
vaprvsapr <vaprvsapr@mail.ru>
2026-07-15 13:10:28 +0300

    Добавил менеджер паролей.

---
+++ EventManager.Application/Security/PasswordManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace EventManager.Application.Security;
+
+public class PasswordManager
+{
+    public string HashPassword(string password)
+    {
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+        return Convert.ToHexString(bytes);
+    }
+
+    public bool CheckPassword(string password, string hash)
+    {
+        var hashedPassword = HashPassword(password);
+        return hashedPassword.Equals(hash, StringComparison.OrdinalIgnoreCase);
+    }
+}
