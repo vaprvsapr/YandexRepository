6708871fd12a647871278f004dd254c5108acfd9
vaprvsapr <vaprvsapr@mail.ru>
2026-07-15 13:38:55 +0300

    Добавил заготовку для TokenGeneratingService.

---
+++ EventManager.Application/Services/TokenGeneratingService.cs
@@ -0,0 +1,37 @@
+﻿using EventManager.Domain.Models;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace EventManager.Application.Services;
+
+public class TokenGeneratingService
+{
+    private readonly string _jwtKey = "secret_key";
+
+    public string GenerateToken(Guid userId, string login, UserRole role)
+    {
+        var claims = new Dictionary<string, object>
+        {
+            [JwtRegisteredClaimNames.Sub] = userId.ToString(),
+            ["login"] = login,
+            ["role"] = role.ToString()
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_jwtKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Claims = claims,
+            Expires = DateTime.UtcNow.AddMinutes(15),
+            IssuedAt = DateTime.UtcNow,
+            SigningCredentials = creds
+        };
+
+        var tokenString = new JsonWebTokenHandler().CreateToken(descriptor);
+
+        return tokenString;
+    }
+}
